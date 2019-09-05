084c42a98ce457581bd6082b95fa62c0cb4b6387
Maikol <maikolsoro.z1998@gmail.com>
2019-09-04 18:27:39 -0600

    CRUD DE PRODUCTOS COMPLETO

--- modelos/productos.modelo.php
+++ modelos/productos.modelo.php
@@ -5,43 +5,14 @@ require_once "conexion.php";
 class ModeloProductos{
 
 	/*=============================================
-	CREAR PRODUCTO
-	=============================================*/
-
-	static public function mdlIngresarProducto($tabla, $datos){
-
-		$stmt = Conexion::conectar()->prepare("INSERT INTO $tabla(producto) VALUES (:producto)");
-
-		$stmt->bindParam(":productos", $datos, PDO::PARAM_STR);
-
-		if($stmt->execute()){
-
-			return "ok";
-
-		}else{
-
-			return "error";
-		
-		}
-
-		$stmt->close();
-		$stmt = null;
-
-	}
-
-
-  
-  
- 
-	/*=============================================
-	MOSTRAR Productos
+	MOSTRAR PRODUCTOS
 	=============================================*/
 
 	static public function mdlMostrarProductos($tabla, $item, $valor){
 
 		if($item != null){
 
-			$stmt = Conexion::conectar()->prepare("SELECT * FROM $tabla WHERE $item = :$item");
+			$stmt = Conexion::conectar()->prepare("SELECT * FROM $tabla WHERE $item = :$item ORDER BY id DESC");
 
 			$stmt -> bindParam(":".$item, $valor, PDO::PARAM_STR);
 
@@ -66,15 +37,19 @@ class ModeloProductos{
 	}
 
 	/*=============================================
-	EDITAR Producto
+	REGISTRO DE PRODUCTO
 	=============================================*/
+	static public function mdlIngresarProducto($tabla, $datos){
 
-	static public function mdlEditarProducto($tabla, $datos){
+		$stmt = Conexion::conectar()->prepare("INSERT INTO $tabla(id_categoria, codigo, descripcion, imagen, stock, precio_compra, precio_venta) VALUES (:id_categoria, :codigo, :descripcion, :imagen, :stock, :precio_compra, :precio_venta)");
 
-		$stmt = Conexion::conectar()->prepare("UPDATE $tabla SET productos = :producto WHERE id = :id");
-
-		$stmt -> bindParam(":productos", $datos["producto"], PDO::PARAM_STR);
-		$stmt -> bindParam(":id", $datos["id"], PDO::PARAM_INT);
+		$stmt->bindParam(":id_categoria", $datos["id_categoria"], PDO::PARAM_INT);
+		$stmt->bindParam(":codigo", $datos["codigo"], PDO::PARAM_STR);
+		$stmt->bindParam(":descripcion", $datos["descripcion"], PDO::PARAM_STR);
+		$stmt->bindParam(":imagen", $datos["imagen"], PDO::PARAM_STR);
+		$stmt->bindParam(":stock", $datos["stock"], PDO::PARAM_STR);
+		$stmt->bindParam(":precio_compra", $datos["precio_compra"], PDO::PARAM_STR);
+		$stmt->bindParam(":precio_venta", $datos["precio_venta"], PDO::PARAM_STR);
 
 		if($stmt->execute()){
 
@@ -92,12 +67,42 @@ class ModeloProductos{
 	}
 
 	/*=============================================
-	BORRAR Producto
+	EDITAR PRODUCTO
+	=============================================*/
+	static public function mdlEditarProducto($tabla, $datos){
+
+		$stmt = Conexion::conectar()->prepare("UPDATE $tabla SET id_categoria = :id_categoria, descripcion = :descripcion, imagen = :imagen, stock = :stock, precio_compra = :precio_compra, precio_venta = :precio_venta WHERE codigo = :codigo");
+
+		$stmt->bindParam(":id_categoria", $datos["id_categoria"], PDO::PARAM_INT);
+		$stmt->bindParam(":codigo", $datos["codigo"], PDO::PARAM_STR);
+		$stmt->bindParam(":descripcion", $datos["descripcion"], PDO::PARAM_STR);
+		$stmt->bindParam(":imagen", $datos["imagen"], PDO::PARAM_STR);
+		$stmt->bindParam(":stock", $datos["stock"], PDO::PARAM_STR);
+		$stmt->bindParam(":precio_compra", $datos["precio_compra"], PDO::PARAM_STR);
+		$stmt->bindParam(":precio_venta", $datos["precio_venta"], PDO::PARAM_STR);
+
+		if($stmt->execute()){
+
+			return "ok";
+
+		}else{
+
+			return "error";
+		
+		}
+
+		$stmt->close();
+		$stmt = null;
+
+	}
+
+	/*=============================================
+	BORRAR PRODUCTO
 	=============================================*/
 
-	static public function mdlBorrarProducto($tabla, $datos){
+	static public function mdlEliminarProducto($tabla, $datos){
 
-		$stmt = Conexion::conectar()->prepare("UPDATE $tabla SET productos = :producto WHERE id = :id");
+		$stmt = Conexion::conectar()->prepare("DELETE FROM $tabla WHERE id = :id");
 
 		$stmt -> bindParam(":id", $datos, PDO::PARAM_INT);
 
