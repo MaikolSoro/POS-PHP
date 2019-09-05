d63a0303fa7bf331ced56ec7daad467be78d94ad
Maikol <maikolsoro.z1998@gmail.com>
2019-09-05 16:32:58 -0600

    registro de clientes y formulario

--- vistas/modulos/clientes.php
+++ vistas/modulos/clientes.php
@@ -2,7 +2,6 @@
 
   <section class="content-header">
     
-    
     <h1>
       
       Administrar clientes
@@ -11,7 +10,7 @@
 
     <ol class="breadcrumb">
       
-      <li><a href="#"><i class="fa fa-dashboard"></i> Inicio</a></li>
+      <li><a href="inicio"><i class="fa fa-dashboard"></i> Inicio</a></li>
       
       <li class="active">Administrar clientes</li>
     
@@ -19,34 +18,226 @@
 
   </section>
 
-  <!-- Main content -->
   <section class="content">
 
-    <!-- Default box -->
     <div class="box">
-      <div class="box-header with-border">
-        <h3 class="box-title">Title</h3>
 
-        <div class="box-tools pull-right">
-          <button type="button" class="btn btn-box-tool" data-widget="collapse" data-toggle="tooltip"
-                  title="Collapse">
-            <i class="fa fa-minus"></i></button>
-          <button type="button" class="btn btn-box-tool" data-widget="remove" data-toggle="tooltip" title="Remove">
-            <i class="fa fa-times"></i></button>
-        </div>
+      <div class="box-header with-border">
+  
+        <button class="btn btn-primary" data-toggle="modal" data-target="#modalAgregarCliente">
+          
+          Agregar clientes
+
+        </button>
+
       </div>
+
       <div class="box-body">
-        Start creating your amazing application!
+        
+       <table class="table table-bordered table-striped dt-responsive tablas">
+         
+        <thead>
+         
+         <tr>
+           
+           <th style="width:10px">#</th>
+           <th>Nombre</th>
+           <th>Cedula</th>
+           <th>Email</th>
+           <th>Teléfono</th>
+           <th>Dirección</th>
+           <th>Fecha nacimiento</th>
+           <th>Total compras</th>
+           <th>Última compra</th>
+           <th>Ingreso al sistema</th>
+           <th>Acciones</th>
+
+         </tr> 
+
+        </thead>
+
+        <tbody>
+          
+          <tr>
+
+            <td>1</td>
+
+            <td>Maikol Soro</td>
+            <td>207850963</td>
+            <td>maikolsoro.z1998@gmail.com</td>
+            <td>86223186</td>
+            <td> 800 mts Santa Rita de Rio Cuarto</td>
+            <td>1998-06-11</td>
+            <td>2019-09-05</td>
+            <td>35</td>
+            <td>2019-09-05 15:45:23</td>
+            
+
+            <td>
+
+              <div class="btn-group">
+                  
+                <button class="btn btn-warning"><i class="fa fa-pencil"></i></button>
+
+                <button class="btn btn-danger"><i class="fa fa-times"></i></button>
+
+              </div>  
+
+            </td>
+
+          </tr>
+
+          
+
+        </tbody>
+
+       </table>
+
       </div>
-      <!-- /.box-body -->
-      <div class="box-footer">
-        Footer
-      </div>
-      <!-- /.box-footer-->
+
     </div>
-    <!-- /.box -->
 
   </section>
-  <!-- /.content -->
+
 </div>
-<!-- /.content-wrapper -->
+
+<!--=====================================
+MODAL Formulario AGREGAR Cliente
+======================================-->
+
+<div id="modalAgregarCliente" class="modal fade" role="dialog">
+  
+  <div class="modal-dialog">
+
+    <div class="modal-content">
+
+      <form role="form" method="post">
+
+        <!--=====================================
+        CABEZA DEL MODAL
+        ======================================-->
+
+        <div class="modal-header" style="background:#3c8dbc; color:white">
+
+          <button type="button" class="close" data-dismiss="modal">&times;</button>
+
+          <h4 class="modal-title">Agregar cliente</h4>
+
+        </div>
+
+        <!--=====================================
+        CUERPO DEL MODAL O LA VENTANA DE REGISTRO
+        ======================================-->
+
+        <div class="modal-body">
+
+          <div class="box-body">
+
+            <!-- ENTRADA PARA EL NOMBRE -->
+            
+            <div class="form-group">
+              
+              <div class="input-group">
+              
+                <span class="input-group-addon"><i class="fa fa-th"></i></span> 
+
+                <input type="text" class="form-control input-lg" name="nuevoCliente" placeholder="Ingresar nombre" required>
+
+              </div>
+
+            </div>
+  
+             <!-- ENTRADA PARA LA CEDULA -->
+            
+             <div class="form-group">
+              
+              <div class="input-group">
+              
+                <span class="input-group-addon"><i class="fa fa-th"></i></span> 
+
+                <input type="number" min ="0" class="form-control input-lg" name="nuevacedula" placeholder="Ingresa la cedula" required>
+
+              </div>
+
+            </div>
+
+             <!-- ENTRADA PARA EL EMAIL -->
+            
+             <div class="form-group">
+              
+              <div class="input-group">
+              
+                <span class="input-group-addon"><i class="fa fa-th"></i></span> 
+
+                <input type="email" class="form-control input-lg" name="nuevoEmail" placeholder="Ingresar  email" required>
+
+              </div>
+
+            </div>
+
+               <!-- ENTRADA PARA EL TELEFONO -->
+            
+               <div class="form-group">
+              
+              <div class="input-group">
+              
+                <span class="input-group-addon"><i class="fa fa-th"></i></span> 
+
+                <input type="text"  class="form-control input-lg" name="nuevoTelefono" placeholder="Ingresar teléfono"  data-inputmask="'mask':'(999) 999-99999'" data-mask required>
+
+              </div>
+
+            </div>
+             <!-- ENTRADA PARA LA DIRECCIÓN -->
+            
+             <div class="form-group">
+              
+              <div class="input-group">
+              
+                <span class="input-group-addon"><i class="fa fa-th"></i></span> 
+
+                <input type="text"  class="form-control input-lg" name="nuevaDireccion" placeholder="Ingresar Dirección" required>
+
+              </div>
+
+            </div>
+
+             <!-- ENTRADA PARA LA FECHA NACIMIENTO-->
+            
+             <div class="form-group">
+              
+              <div class="input-group">
+              
+                <span class="input-group-addon"><i class="fa fa-th"></i></span> 
+
+                <input type="text" class="form-control input-lg" name="nuevaFechaNacimiento" placeholder="Ingresar fecha nacimiento" data-inputmask="'alias':'yyyy/mm/dd'" data-mask required>
+
+              </div>
+
+            </div>
+
+          </div>
+
+        </div>
+
+        <!--=====================================
+        PIE DEL MODAL
+        ======================================-->
+
+        <div class="modal-footer">
+
+          <button type="button" class="btn btn-default pull-left" data-dismiss="modal">Salir</button>
+
+          <button type="submit" class="btn btn-primary">Guardar cliente</button>
+
+        </div>
+
+      </form>
+
+    </div>
+
+  </div>
+
+</div>
+
+
